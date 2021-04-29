f48a7f90ab77b2a82ea7d69acc32feda2531a375
laapush <buesra.apaydin@hs-furtwangen.de>
2021-04-29 14:54:06 +0200

    comm

--- register.php
+++ register.php
@@ -52,6 +52,7 @@ if (isset($_POST['submit'])) {
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
 
 	<link rel="stylesheet" type="text/css" href="style.css">
 
--- request.php
+++ request.php
@@ -4,10 +4,10 @@ session_start();
 
 if (!isset($_SESSION['vname'])) {
     header("Location: login.php");
-
 }
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,84 +16,85 @@ if (!isset($_SESSION['vname'])) {
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style2.css">
     <link rel="stylesheet" href="style3.css">
-   <script src="https://kit.fontawesome.com/13c7fa4ddd.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/13c7fa4ddd.js" crossorigin="anonymous"></script>
 
     <title>Loan Broker - Kreditanfrage</title>
+
 </head>
 
 <body>
-<nav>
+  <nav>
 
-<div id="logo"><a style="text-decoration: none;
-    color: #68a6da;" href="welcome.php">
-         <h4><i class="fab fa-ethereum"></i>LOAN BROKER</h4></a>
+  <div id="logo"><a style="text-decoration: none;
+      color: #68a6da;" href="welcome.php">
+          <h4><i class="fab fa-ethereum"></i>LOAN BROKER</h4></a>
+      </div>
+      <ul class="nav-links">
+      <li> <a href="welcome.php">Startseite</a> </li>
+      <li> <a href="request.php">Kreditanfrage</a> </li>
+      <li> <a href="anfragen.php">Meine Anfragen</a> </li>
+      <li> <a href="angebote.php">Meine Angebote</a> </li>
+      <li> <a href="logout.php">Logout</a> </li>
+      </ul>
+      <div class="burger">
+          <div class="line1"></div>
+          <div class="line2"></div>
+          <div class="line3"></div>
     </div>
-    <ul class="nav-links">
-    <li> <a href="welcome.php">Startseite</a> </li>
-    <li> <a href="request.php">Kreditanfrage</a> </li>
-     <li> <a href="anfragen.php">Meine Anfragen</a> </li>
-    <li> <a href="angebote.php">Meine Angebote</a> </li>
-    <li> <a href="logout.php">Logout</a> </li>
-    </ul>
-    <div class="burger">
-        <div class="line1"></div>
-        <div class="line2"></div>
-        <div class="line3"></div>
-    </div>
-</nav>
-<div class="body">
-   <div class=title>
-    <h1>Kreditanfrage</h1>
+  </nav>
+  <div class="body">
+    <div class=title>
+      <h1>Kreditanfrage</h1>
     </div>
 
     <main class="anfrage">
-  <div class="anfrage-wrapper">
-    <form action="angebote.php"method="post" class="anfrage-form">
+     <div class="anfrage-wrapper">
+       <form action="angebote.php"method="post" class="anfrage-form">
 
-      <label for="amount">Nettokreditbetrag:</label>
-      <input type="number" id="amount" name ="amount"min="100" max="100000" <!--pattern="[100-100000]-->"maxlength="6"  placeholder="Euro" required>       </input><br>
+        <label for="amount">Nettokreditbetrag:</label>
+        <input type="number" id="amount" name ="amount"min="100" max="100000" <!--pattern="[100-100000]-->"maxlength="6"  placeholder="Euro" required>       </input><br>
 
-      <label for="term">Kreditlaufzeit:</label>
-      <input type="number" id="term" name="term" placeholder="Monate" min="1" max="36" <!--pattern="[1-36]{2} -->"maxlength="2" required>    </input><br>
+        <label for="term">Kreditlaufzeit:</label>
+        <input type="number" id="term" name="term" placeholder="Monate" min="1" max="36" <!--pattern="[1-36]{2} -->"maxlength="2" required>    </input><br>
 
-      <label for="ssn">SSN:   </label>
-        <select class="anfrage-form2" name="ssn" required>
-        <option value="testA">A</option>
-        <option value="testB">B</option>
-        <option value="testC">C</option>
-        <option value="testD">D</option>
-        <option value="testE">E</option>
-        <option value="testF">F</option>
-        <option value="testG">G</option>
-        <option value="testH">H</option>
-        <option value="testI">I</option>
-        <option value="testJ">J</option>
-        <option value="testK">K</option>
-        <option value="testL">L</option>
-        <option value="testN">N</option>
-        <option value="testO">O</option>
-        <option value="testP">P</option>
-        </select>
+        <label for="ssn">SSN:   </label>
+          <select class="anfrage-form2" name="ssn" required>
+          <option value="testA">A</option>
+          <option value="testB">B</option>
+          <option value="testC">C</option>
+          <option value="testD">D</option>
+          <option value="testE">E</option>
+          <option value="testF">F</option>
+          <option value="testG">G</option>
+          <option value="testH">H</option>
+          <option value="testI">I</option>
+          <option value="testJ">J</option>
+          <option value="testK">K</option>
+          <option value="testL">L</option>
+          <option value="testN">N</option>
+          <option value="testO">O</option>
+          <option value="testP">P</option>
+          </select>
 
-      <label>Optionen:</label>
-      <div>
-      <label style="font-weight:normal; font-size:small">Nur die niedrigste Rate erhalten<input type="checkbox" id="lowest" name="lowest"></label>
-    </div>
-    <div>   <label style="font-weight:normal; font-size:small">E-Mail Benachrichtigung<input type="checkbox" id="notify" name="notify"></label>
-    </div>
-      <button type="submit" onclick="myFunction()" class="button">Absenden</button>
+        <label>Optionen:</label>
+         <div>
+          <label style="font-weight:normal; font-size:small">Nur die niedrigste Rate erhalten<input type="checkbox" id="lowest" name="lowest"></label>
+         </div>
+         <div><label style="font-weight:normal; font-size:small">E-Mail Benachrichtigung<input type="checkbox" id="notify" name="notify"></label>
+        </div>
+         <button type="submit" onclick="myFunction()" class="button">Absenden</button>
     </form>
   </div>
-</main>
+  </main>
 </div>
 <script>
-function myFunction() {
-  var t =setTimeout("alertMsg()",2000);
-}
-function alertMsg()
-{
-    alert("Die Kreditanfrage wurde verschickt.");
-}
+  function myFunction() {
+    var t =setTimeout("alertMsg()",2000);
+  }
+  function alertMsg()
+  {
+      alert("Die Kreditanfrage wurde verschickt.");
+  }
 </script>
 </body>
 </html>
